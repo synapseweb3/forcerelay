5a3a1b97b7ff0fdece0e26c0fdf1c59465ac9682
fjchen7 <fjchen7@gmail.com>
2022-11-30 17:15:22 +0800

    Add method `run` on EthEventMonitor

--- crates/relayer/src/chain/eth/event/monitor.rs
+++ crates/relayer/src/chain/eth/event/monitor.rs
@@ -1,43 +1,44 @@
 use std::sync::Arc;
 
+use crossbeam_channel as channel;
+use ethers::prelude::{abigen, Provider, SignerMiddleware, StreamExt, Ws};
 use ethers::signers::Wallet;
-use ethers::types::{Log, Address};
-use ethers::prelude::{
-    abigen, Provider, SignerMiddleware, StreamExt, Ws,
-};
+use ethers::types::{Address, Log};
 use ethers_contract::stream::EventStream;
 use ethers_contract::ContractError;
 use ethers_providers::FilterWatcher;
 
 use k256::ecdsa::SigningKey;
 use tokio::runtime::Runtime as TokioRuntime;
+use tracing::debug;
 
-use crate::event::monitor::Next;
+use crate::event::monitor::{MonitorCmd, Next};
 
-// type SubscriptionStream = EventStream<'static, FilterWatcher<'static, Ws, Log>, EthEventMonitor, ContractError<Client>>;
-type SubscriptionStream<'a> = EventStream<
+type _SubscriptionStream<'a> = EventStream<
     'a,
     FilterWatcher<'a, Ws, Log>,
     IBCEvents,
     ContractError<SignerMiddleware<Provider<Ws>, Wallet<SigningKey>>>,
 >;
 
-// type Query =
 type Client = Provider<Ws>;
 
 abigen!(IBC, "./src/chain/eth/IBC.json");
-pub struct EthEventMonitor<'a> {
+
+#[derive(Clone, Debug)]
+pub struct EthEventMonitor {
     client: Client,
     // event_queries: Vec<IBC<SignerMiddleware<Provider<Ws>, Wallet<SigningKey>>>>,
-    _subscription: Box<SubscriptionStream<'a>>,
+    // _subscription: Box<SubscriptionStream<'a>>,
     rt: Arc<TokioRuntime>,
     _chain_id: u64,
     address: Address,
     wallet: Wallet<SigningKey>,
     start_block_number: u64,
+    rx_cmd: channel::Receiver<MonitorCmd>,
 }
 
-impl<'a> EthEventMonitor<'a> {
+impl EthEventMonitor {
     // pub fn new(
     //     chain_id: ChainId,
     //     node_addr: Url,
@@ -65,29 +66,47 @@ impl<'a> EthEventMonitor<'a> {
     //     Ok(())
     // }
 
+    #[allow(clippy::while_let_loop)]
+    pub fn run(mut self) {
+        debug!("starting event monitor");
+        loop {
+            match self.run_loop() {
+                Next::Continue => continue,
+                Next::Abort => break,
+            }
+        }
+        debug!("event monitor is shutting down");
+        // TODO: close client
+    }
 
+    #[allow(clippy::while_let_loop)]
     pub fn run_loop(&mut self) -> Next {
         let signer = SignerMiddleware::new(self.client.clone(), self.wallet.clone());
         let ibc = Arc::new(IBC::new(self.address, Arc::new(signer)));
         let ibc_events = Arc::clone(&ibc);
 
+        if let Ok(MonitorCmd::Shutdown) = self.rx_cmd.try_recv() {
+            return Next::Abort;
+        }
         let rt = self.rt.clone();
         rt.block_on(async move {
-            if let Ok(stream) = ibc_events.events().from_block(self.start_block_number).stream().await {
+            if let Ok(stream) = ibc_events
+                .events()
+                .from_block(self.start_block_number)
+                .stream()
+                .await
+            {
                 let mut meta_stream = stream.with_meta();
                 while let Some(Ok((event, meta))) = meta_stream.next().await {
                     if meta.block_number > self.start_block_number.into() {
                         // TODO: convert eth event to IBC Event
+                        // TODO: send msg to channel
                         println!("[event] = {:?}", event);
                         println!("[event_meta] = {:?}\n", meta);
                     }
                 }
             }
         });
-
-
-        todo!()
-
+        return Next::Continue;
     }
-
 }
