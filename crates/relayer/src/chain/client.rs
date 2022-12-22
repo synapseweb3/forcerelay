5c929492a1c07231c5c4c99c80036fd4c8dc39d5
ImJeremyHe <yiliang.he@qq.com>
2022-12-22 11:06:40 +0800

    fix: fix eth chain endpoint

--- crates/relayer/src/chain/client.rs
+++ crates/relayer/src/chain/client.rs
@@ -4,13 +4,15 @@ use crate::chain::cosmos;
 use crate::config::ChainConfig;
 use crate::foreign_client::CreateOptions;
 
+use super::ChainType;
+
 /// Client parameters for the `build_create_client` operation.
 ///
 /// The parameters are specialized for each supported chain type.
 #[derive(Clone, Debug)]
 pub enum ClientSettings {
     Tendermint(cosmos::client::Settings),
-    Eth,
+    Other,
 }
 
 impl ClientSettings {
@@ -22,13 +24,18 @@ impl ClientSettings {
         src_chain_config: &ChainConfig,
         dst_chain_config: &ChainConfig,
     ) -> Self {
-        // Currently, only Tendermint chain pairs are supported by
-        // ForeignClient::build_create_client_and_send. Support for
-        // heterogeneous chains is left for future revisions.
-        ClientSettings::Tendermint(cosmos::client::Settings::for_create_command(
-            options,
-            src_chain_config,
-            dst_chain_config,
-        ))
+        match (src_chain_config.r#type(), dst_chain_config.r#type()) {
+            (ChainType::CosmosSdk, ChainType::CosmosSdk) => {
+                // Currently, only Tendermint chain pairs are supported by
+                // ForeignClient::build_create_client_and_send. Support for
+                // heterogeneous chains is left for future revisions.
+                ClientSettings::Tendermint(cosmos::client::Settings::for_create_command(
+                    options,
+                    src_chain_config,
+                    dst_chain_config,
+                ))
+            }
+            _ => ClientSettings::Other,
+        }
     }
 }
--- crates/relayer/src/chain/cosmos.rs
+++ crates/relayer/src/chain/cosmos.rs
@@ -1831,7 +1831,7 @@ impl ChainEndpoint for CosmosSdkChain {
     ) -> Result<Self::ClientState, Error> {
         let settings = match settings {
             ClientSettings::Tendermint(settings) => settings,
-            ClientSettings::Eth => return Err(Error::build_client_state_failure()),
+            ClientSettings::Other => return Err(Error::build_client_state_failure()),
         };
         let unbonding_period = self.unbonding_period()?;
         let trusting_period = settings
