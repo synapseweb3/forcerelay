6a49138ffae6c85a926153db9f49f24cdc26cf58
fjchen7 <fjchen7@gmail.com>
2022-12-29 18:26:07 +0800

    send eth header by shard

--- crates/relayer/src/supervisor/forcerelay.rs
+++ crates/relayer/src/supervisor/forcerelay.rs
@@ -1,14 +1,17 @@
 use ibc_relayer_types::events::IbcEvent;
-use tracing::{error, info};
+use tracing::{error, info, warn};
 
 use crate::chain::handle::ChainHandle;
 use crate::chain::requests::{PageRequest, QueryClientStatesRequest};
 use crate::chain::tracking::{NonCosmosTrackingId, TrackedMsgs, TrackingId};
+use crate::client_state::IdentifiedAnyClientState;
 use crate::config::ChainConfig;
 use crate::error::ErrorDetail::LightClientVerification;
 use crate::event::monitor::EventBatch;
 use tendermint_light_client::errors::ErrorDetail::MissingLastBlockId;
 
+const MAX_HEADERS_IN_BATCH: u64 = 10;
+const MAX_RETRY_NUMBER: u8 = 5;
 pub fn handle_event_batch<ChainA: ChainHandle, ChainB: ChainHandle>(
     eth_chain: &ChainA,
     ckb_chain: &ChainB,
@@ -19,9 +22,9 @@ pub fn handle_event_batch<ChainA: ChainHandle, ChainB: ChainHandle>(
     if !matches!(src_chain.config().unwrap(), ChainConfig::Eth(_))
         || !matches!(dst_chain.config().unwrap(), ChainConfig::Ckb(_))
     {
-        error!("{:?}", src_chain);
-        error!("{:?}", dst_chain);
         error!("ignore header relay while src chain is not eth or dst chain is not ckb");
+        error!("src_chain: {:?}", src_chain);
+        error!("dst_chain: {:?}", dst_chain);
         return;
     }
 
@@ -46,7 +49,7 @@ pub fn handle_event_batch<ChainA: ChainHandle, ChainB: ChainHandle>(
             }
             // returned err indicates headers falling behind
             let err = ret.unwrap_err();
-            let start_height = match err.detail() {
+            let mut start_height = match err.detail() {
                 LightClientVerification(e) => match &e.source {
                     MissingLastBlockId(height) => height.height.into(),
                     _ => u64::MAX,
@@ -57,48 +60,68 @@ pub fn handle_event_batch<ChainA: ChainHandle, ChainB: ChainHandle>(
                 error!("receive unexpected error: {:?}", err);
                 return;
             }
-            let limit = height.revision_height() - start_height + 1;
-            let request = QueryClientStatesRequest {
-                pagination: Some(PageRequest {
-                    offset: start_height,
-                    limit,
-                    ..Default::default()
-                }),
-            };
-            let client_states = src_chain.query_clients(request).unwrap();
-            let len = client_states.len() as u64;
-            info!(
-                "get ETH headers from {} to {}",
-                start_height,
-                start_height + len - 1
-            );
-            let tracked_msgs = TrackedMsgs {
-                msgs: client_states
-                    .into_iter()
-                    .map(|s| s.client_state.into())
-                    .collect(),
-                tracking_id: TrackingId::Static(NonCosmosTrackingId::ETH_UPDATE_CLIENT),
-            };
-            let ret = dst_chain.send_messages_and_wait_commit(tracked_msgs);
-            match ret {
-                Ok(_) => {
-                    info!(
-                        "ETH headers from {} to {} are relayed to CKB",
-                        start_height,
-                        start_height + len - 1
+
+            let target_height = height.revision_height();
+            let mut retry_number = 0;
+            while start_height < target_height {
+                if retry_number != 0 {
+                    info!("{} time retry from height {}", retry_number, start_height);
+                }
+                let limit = std::cmp::min(MAX_HEADERS_IN_BATCH, target_height - start_height + 1);
+                let request = QueryClientStatesRequest {
+                    pagination: Some(PageRequest {
+                        offset: start_height,
+                        limit,
+                        ..Default::default()
+                    }),
+                };
+                let client_states = src_chain.query_clients(request).unwrap();
+                let len = client_states.len() as u64;
+                let end_height = start_height + len - 1;
+                info!("get ETH headers from {} to {}", start_height, end_height);
+                if len < limit {
+                    warn!(
+                        "can't find enough ETH headers to relay, expect {} but only fetch {}",
+                        limit, len
                     );
-                    if len < limit {
-                        info!("warning: can't find enought ETH header to relay");
+                }
+                let ret = send_messages(dst_chain, client_states);
+                match ret {
+                    Ok(_) => {
+                        info!(
+                            "ETH headers from {} to {} are relayed to CKB",
+                            start_height, end_height
+                        );
+                        retry_number = 0;
+                        start_height = end_height + 1;
+                    }
+                    Err(e) => {
+                        error!("encounter error when relaying ETH header: {:?}", e);
+                        retry_number += 1;
                     }
                 }
-                Err(_) => {
+                if retry_number >= MAX_RETRY_NUMBER {
                     error!(
-                        "encounter error when relaying ETH header: {:?}",
-                        ret.unwrap_err()
+                        "retry number {} exceeds maximum value {}. stop header sync.",
+                        retry_number, MAX_RETRY_NUMBER
                     );
-                    // TODO: how to handle fails of sending header?
+                    return;
                 }
             }
         }
     }
+}
+
+fn send_messages<Chain: ChainHandle>(
+    chain: &Chain,
+    client_states: Vec<IdentifiedAnyClientState>,
+) -> Result<Vec<crate::event::IbcEventWithHeight>, crate::error::Error> {
+    let tracked_msgs = TrackedMsgs {
+        msgs: client_states
+            .into_iter()
+            .map(|s| s.client_state.into())
+            .collect(),
+        tracking_id: TrackingId::Static(NonCosmosTrackingId::ETH_UPDATE_CLIENT),
+    };
+    return chain.send_messages_and_wait_commit(tracked_msgs);
 }
