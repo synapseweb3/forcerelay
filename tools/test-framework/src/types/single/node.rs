ad29dc9373d6fef28662e2b411c2bc9e0aa8b0e0
jjy <jjyruby@gmail.com>
2023-08-24 17:13:03 +0800

    fix: ckb4ibc chain can't find axon lightclient

--- tools/test-framework/src/types/single/node.rs
+++ tools/test-framework/src/types/single/node.rs
@@ -121,9 +121,9 @@ impl<'a, Chain> TaggedFullNodeExt<Chain> for MonoTagged<Chain, &'a FullNode> {
     }
 }
 
-fn hex_to_h256(h: &[u8]) -> [u8; 32] {
+fn hex_to_h256(h: &[u8]) -> ckb_fixed_hash::H256 {
     let raw = hex::decode(h).expect("decode hex");
-    raw.try_into().expect("convert to h256")
+    ckb_fixed_hash::H256(raw.try_into().expect("convert to h256"))
 }
 
 impl FullNode {
@@ -148,16 +148,23 @@ impl FullNode {
     ) -> Result<config::ChainConfig, Error> {
         let ckb_rpc = Url::from_str(self.chain_driver.rpc_address().as_str())?;
         let connection_type_args =
-            hex_to_h256(b"f49ce32397c6741998b04d7548c5ed372007424daf67ee5bfadaefec3c865781").into();
+            hex_to_h256(b"f49ce32397c6741998b04d7548c5ed372007424daf67ee5bfadaefec3c865781");
         let channel_type_args =
-            hex_to_h256(b"b407c3b93dee611b2e65248254c28012a8d227c53803e5842d4a81934179adfc").into();
+            hex_to_h256(b"b407c3b93dee611b2e65248254c28012a8d227c53803e5842d4a81934179adfc");
         let packet_type_args =
-            hex_to_h256(b"63b3d51df3884cc649135a51ad2a1ae1a8c2dfeca37c8b16220b26716fb3b4c4").into();
+            hex_to_h256(b"63b3d51df3884cc649135a51ad2a1ae1a8c2dfeca37c8b16220b26716fb3b4c4");
         let client_cell_type_args =
-            hex_to_h256(b"7ede7d98985de2f464e737b8e177ede186c50d3d584d1bd9b2399330c2187e61").into();
+            hex_to_h256(b"7ede7d98985de2f464e737b8e177ede186c50d3d584d1bd9b2399330c2187e61");
         let mut onchain_light_clients = HashMap::default();
         onchain_light_clients.insert(
             ClientType::Ckb4Ibc,
+            LightClientItem {
+                chain_id: self.chain_driver.chain_id.clone(),
+                client_cell_type_args: client_cell_type_args.clone(),
+            },
+        );
+        onchain_light_clients.insert(
+            ClientType::Axon,
             LightClientItem {
                 chain_id: self.chain_driver.chain_id.clone(),
                 client_cell_type_args,
