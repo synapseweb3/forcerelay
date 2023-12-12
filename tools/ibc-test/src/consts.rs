ea00c523c48a66046fe23fd06db10b59bd6445ae
liyukun <liyukun@cryptape.com>
2023-12-12 09:46:13 +0800

    chore: enable contracts that remove a-instructions

--- tools/ibc-test/src/consts.rs
+++ tools/ibc-test/src/consts.rs
@@ -5,18 +5,18 @@ pub const SUDT_CODE_HASH: H256 =
 pub const CONNECTION_CODE_HASH: H256 =
     h256!("0xe802fa98adeed078eb39225f6d7759ceeeecc32fa28942f2d2c6a158ec40cebb");
 pub const CHANNEL_CODE_HASH: H256 =
-    h256!("0x4eb3ffbbf4a2b0a14da17d4951860ab6ae6ebd654515ea58cdb5a62b4c0c5cf7");
+    h256!("0xa666ecfbe673fe3bfb7a60ce52db78da2f24bc3d65118906e5ef96d299e81bf8");
 
 pub const SUDT_TYPE_ARGS: H256 =
     h256!("0xf49ce32397c6741998b04d7548c5ed372007424daf67ee5bfadaefec3c865781");
 pub const CONNECTION_TYPE_ARGS: H256 =
     h256!("0x9d46ad77b8f5107d5898d7e7ad82be206c4fd95f562004efe32488605e8846c7");
 pub const CHANNEL_TYPE_ARGS: H256 =
-    h256!("0x39f75db1c443ef884dac56482bf2912054cf7989eaf2c71cf44b0df91cdd86b0");
+    h256!("0x50179971499a451d06059365ae559329afcc48a3b7a53af5310d906c0e22f16d");
 pub const PACKET_TYPE_ARGS: H256 =
-    h256!("0x56bd6db5728d711559bda25121e13e1d5d39157ac62b45824b87534b3228d3e8");
+    h256!("0x901959f4e4b8b8c8aa79bdad1e68742d73bcd7f510599249bb46c1923178bd0f");
 pub const CLIENT_TYPE_ARGS: H256 =
-    h256!("0xcdaf5ce4d6f41c98b7abaf417b867fe78778432eabaf06ee174144e63195c2d6");
+    h256!("0x2f3efdcdc1ae5327187920eec83da80bdaa11c581d47aee0eabc895e9ac713e8");
 
 // Don't know how to pass this from axon to ckb. It doesn't change often, so
 // let's hardcode it for now.
